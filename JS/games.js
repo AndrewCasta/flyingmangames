65a2b7f7276723f8c81915116499ec01b768239f
Andrew <acasta4704@gmail.com>
2021-10-06 17:53:53 +1100

    initial commit

---
+++ JS/games.js
@@ -0,0 +1,12 @@
+// Collection data structure for the games
+// Info for overview & individual product
+
+const games = [
+  {
+    id: 1,
+    name: 'Example Game',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat quibusdam minima eligendi minus. Incidunt, sit itaque? Tempore quaerat minima nobis laudantium commodi ea mollitia a obcaecati omnis? Eius aspernatur voluptas quisquam, culpa assumenda incidunt vero quis illum perspiciatis in sunt dolores maxime consequatur est optio delectus magni reprehenderit laborum voluptates.',
+    externalLink: 'patreon.com',
+  },
+];
