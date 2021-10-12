ee07ff9cb436f059e41b27a417a0703702958395
Andrew <acasta4704@gmail.com>
2021-10-12 17:29:16 +1100

    Add our games section Add mailing list & simple footer Add misc images

--- JS/main.js
+++ JS/main.js
@@ -2,10 +2,37 @@
 // Nav
 // ============
 
-const mobileNavLinks = document.querySelector('.nav-links-mobile');
+const mobileNavLinks = document.querySelector('.nav-links');
 const navToggle = document.querySelector('#nav-toggle');
 
 navToggle.addEventListener('click', e => {
   e.preventDefault;
   mobileNavLinks.classList.toggle('hidden');
 });
+
+// ============
+// Form Submit
+// ============
+
+var form = document.getElementById('mailing-form');
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById('mailing-form-status');
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then(response => {
+      status.innerHTML = 'Thanks for your submission!';
+      form.reset();
+    })
+    .catch(error => {
+      status.innerHTML = 'Oops! There was a problem submitting your form';
+    });
+}
+form.addEventListener('submit', handleSubmit);
